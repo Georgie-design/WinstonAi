1248994b15bc52765a1ee4f10d6d48263f6b7347
Jack Jensen <georgieinternational123@gmail.com>
2023-07-26 14:46:44 -0700

    Auth and Customilization

--- WinstonUI/views.py
+++ WinstonUI/views.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import User
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        pass
+ 
+
+    first_name = current_user.first_name
+    return render_template("home.html", user=current_user, name=first_name)
+
+
+
